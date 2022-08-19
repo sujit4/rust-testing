0bde01c39d911dcc2f347bf72b2c32e15e17d774
Sujit Konapur <satwork13@gmail.com>
2022-08-19 13:54:48 -0700

    simple test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    println!("Please enter your name: ");
+    std::io::stdin().read_line(&mut line).unwrap();
+    println!("{}", build_message(line));
+}
+
+fn build_message(name: String) -> String{
+    let message = "You are welcome ".to_owned() + &name;
+    return message;
+}
+
+#[test]
+fn test_build_message(){
+    let name = String::from("Fikayo");
+
+    assert_eq!(build_message(name), "You are welcome Fikayo")
 }
